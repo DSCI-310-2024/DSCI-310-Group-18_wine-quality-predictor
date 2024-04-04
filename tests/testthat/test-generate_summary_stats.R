377c8e6f41c48c5d2cdc8d84190fb355535ce7ed
Alexander Dawson <xdwsn1@gmail.com>
2024-04-03 23:18:07 -0700

    completed initial test suite implementation for generate_summary_stats() function

--- tests/testthat/test-generate_summary_stats.R
+++ tests/testthat/test-generate_summary_stats.R
@@ -2,26 +2,35 @@ library(testthat)
 library(tidyverse)
 source("../../R/generate_summary_stats.R")
 
-test_that("'generate_summary_stats' should return a data frame (or extension, eg: a tibble).", {
-    # add tests here
-})
+summary_stats_min <- generate_summary_stats(minimal_data, var1)
+summary_stats_test <- generate_summary_stats(test_data, length)
 
 test_that("'generate_summary_stats' should return a data frame or extension with n + 2 columns, where n
 is the number of columns in the original input data frame.", {
-    # add tests here
+   expect_identical(minimal_data_output_col_count, count_classes(summary_stats_min))
+   expect_identical(test_data_output_col_count, count_classes(summary_stats_test))
 })
 
 test_that("'generate_summary_stats' should return a data frame or extension where values in the 'count'
  column represent the number of observations in each group_var column, from the original data frame.", {
-    # add tests here
+   expect_identical(test_data_output_counts, summary_stats_test %>% select(count))
+   expect_identical(minimal_data_output_counts, summary_stats_min %>% select(count))
  })
 
  test_that("'generate_summary_stats' should return a data frame or extension where values in the 'percentage'
  column represent the percentage of observations in the group_var column, from the total observation count.", {
-    # add tests here
+   expect_identical(test_data_output_percentages, summary_stats_test %>% select(percentage))
+   expect_identical(minimal_data_output_percentages, summary_stats_min %>% select(percentage))
  })
+
+test_that("'generate_summary_stats' should format a new data frame output according to the examples in the helper
+document.", {
+   expect_identical(minimal_data_output, summary_stats_min)
+   expect_identical(test_data_output, summary_stats_test)
+})
 
  test_that("'generate_summary_stats' should throw an error message when incorrect types are passed to 'data' and
  'group_var' arguments.", {
-    # add tests here
+   expect_error(generate_summary_stats(list_data, 1))
+   expect_error(generate_summary_stats(minimal_data, NULL))
  })
