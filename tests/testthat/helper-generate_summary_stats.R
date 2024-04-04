2ddcf1cda88ad7ca9e02af44ded6ac3ca063390d
Alexander Dawson <xdwsn1@gmail.com>
2024-04-03 22:44:48 -0700

    completed data implementation for generate_summary_stats function tests

--- tests/testthat/helper-generate_summary_stats.R
+++ tests/testthat/helper-generate_summary_stats.R
@@ -6,16 +6,20 @@ minimal_data <- data.frame("var1", "var2")
 
 list_data <- list(1, 2, 3, 4, 5, 6, 7, 8, 9)  #shouldn't work with the function, should cause argument type error for data
 
-test_data <- data.frame("length" = c(1, 2, 3, 4, 5, 6, 7, 8, 9),
+test_data <- data.frame("length" = c(1, 2, 2, 3, 3, 3, 3, 4, 4),
                         "width" = c(90, 80, 70, 60, 50, 40, 30, 20, 10),
                         "height" = c(10, 20, 30, 40, 50, 60, 70, 80, 90))
 
 
 # expected function outputs
 
-minimal_data_output <- data.frame("var1" = NULL, 
+minimal_data_output <- data.frame("var1" = NA, 
                                   "count" = 0, 
                                   "percentage" = 100, 
-                                  "var2" = NULL)
+                                  "var2" = NA)
 
-test_data
+test_data <- data.frame("length" = c(1, 2, 3, 4),
+                        "count" = c(1, 2, 4, 2),
+                        "percentage" = c(11.1, 22.2, 44.4, 22.2),
+                        "width_avg" = c(90, 75, 45, 15),
+                        "height_avg" = c(10, 25, 55, 85))
